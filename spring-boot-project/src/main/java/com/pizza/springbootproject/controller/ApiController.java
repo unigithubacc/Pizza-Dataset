a86c2f94cb9194685188b002f669571e521111d1
unigithubacc <aimforthemoon.00@gmail.com>
2024-04-21 15:40:31 +0200

    Add spring-boot-starter-web and springdoc-openapi-ui dependencies to pom.xml Update application.properties with database and Swagger configurations

---
+++ spring-boot-project/src/main/java/com/pizza/springbootproject/controller/ApiController.java
@@ -0,0 +1,16 @@
+package com.pizza.springbootproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+    
+    @GetMapping
+    public String getApiData() {
+        return "Hallo von der API!";
+    }
+
+}
---
+++ spring-boot-project/src/main/java/com/pizza/springbootproject/service/ManagerService.java
@@ -0,0 +1,5 @@
+package com.pizza.springbootproject.service;
+
+public class ManagerService {
+    
+}
