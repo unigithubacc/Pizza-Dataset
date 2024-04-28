35f6d992221902c01a40ebfe126fa7eb64d24175
unigithubacc <aimforthemoon.00@gmail.com>
2024-04-28 23:00:06 +0200

    Update pom.xml with springfox-swagger2, springfox-swagger-ui, and springdoc-openapi-starter-webmvc-ui dependencies

--- spring-boot-project/src/main/java/com/pizza/springbootproject/security/securityConfig.java
+++ spring-boot-project/src/main/java/com/pizza/springbootproject/security/securityConfig.java
@@ -6,10 +6,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 @EnableWebSecurity
 public class securityConfig {
+    @Bean
+    public Docket SwaggerApi(){
+        
+        return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build();
+    }
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // Die Cross-Site-Request-Forgery-Schutz wird deaktiviert
