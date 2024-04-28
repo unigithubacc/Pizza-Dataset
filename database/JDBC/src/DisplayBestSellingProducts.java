46ee1b7742421a37daf0e8de7dbd9b36200fafbf
unigithubacc <aimforthemoon.00@gmail.com>
2024-04-28 14:17:48 +0200

    SQL Abfrage für die 5 meistverkauften Produkte hinzugefügt

---
+++ database/JDBC/src/DisplayBestSellingProducts.java
@@ -0,0 +1,40 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DisplayBestSellingProducts {
+    static final String DB_URL = "jdbc:mysql://localhost:3306/pizza";
+    static final String USER = "root";
+    static final String PASS = "ProLab895+";
+
+    public static void main(String[] args) {
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+             Statement stmt = conn.createStatement();) {
+            String sql = "SELECT products.SKU, products.Name, SUM(orders.nItems) AS TotalSold "+
+                         "FROM pizza.products "+
+                         "INNER JOIN pizza.orderitems ON products.SKU = orderitems.SKU "+
+                         "INNER JOIN pizza.orders ON orderitems.orderID = orders.orderID "+
+                         "GROUP BY products.SKU, products.Name "+
+                         "ORDER BY TotalSold DESC "+
+                         "LIMIT 5 "; 
+            
+        
+            ResultSet rs = stmt.executeQuery(sql);
+            System.out.println("Die meistverkauften Produkte:");
+
+            while (rs.next()) {
+                String SKU = rs.getString("SKU");
+                String name = rs.getString("Name");
+                int totalSold = rs.getInt("TotalSold");
+            
+                System.out.println("SKU: " + SKU + ", Name: " + name +
+                                   ", Gesamtmenge verkauft: " + totalSold);
+            }
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
