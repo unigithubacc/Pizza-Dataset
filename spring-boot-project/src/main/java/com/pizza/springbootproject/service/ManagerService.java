8869bc36bfc3cccf675920aa53ad2a64a401f832
unigithubacc <aimforthemoon.00@gmail.com>
2024-04-28 19:08:42 +0200

    Add ManagerService class and update application.properties with API URL

--- spring-boot-project/src/main/java/com/pizza/springbootproject/service/ManagerService.java
+++ spring-boot-project/src/main/java/com/pizza/springbootproject/service/ManagerService.java
@@ -1,12 +1,17 @@
 package com.pizza.springbootproject.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.pizza.springbootproject.dao.ManagerDao;
 
 @Service
 public class ManagerService {
+
+    // Read the URL of the external API from properties file.
+    @Value("${ManagerService.url}")
+    String apiUrl;
 
     @Autowired
     private ManagerDao managerDao;
