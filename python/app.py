fc452cc413a7398a7ae34c2ff80a0c2fbb4f36f5
unigithubacc <aimforthemoon.00@gmail.com>
2024-05-30 12:28:10 +0200

    refactor: Add CSS file loading function to improve styling in app.py

--- python/app.py
+++ python/app.py
@@ -4,6 +4,11 @@ from frontend.page2 import main as page2_main
 from frontend.page3 import main as page3_main
 from frontend.page4 import main as page4_main
 
+# Lade die CSS-Datei
+def load_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
 # Dictionary, das die Seiten-Funktionen speichert
 PAGES = {
     "Seite 1": page1_main,
@@ -11,6 +16,9 @@ PAGES = {
     "Seite 3": page3_main,
     "Seite 4": page4_main
 }
+
+# CSS-Datei laden
+load_css("frontend\style.css")
 
 # Navbar mit einem Dropdown-Menü
 st.sidebar.title("Navigation")
