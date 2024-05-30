384bde08d85b78115e1633075bf2b46e1303eb73
unigithubacc <aimforthemoon.00@gmail.com>
2024-05-30 13:54:28 +0200

    feat: Add lazy loading for images and improve page load
    refactor: Fix file path for loading CSS in app.py
    
    feat: Add size column to StoreModel and modify SQL query for top selling products
    
    refactor: Add CSS file loading function to improve styling in app.py
    
    feat: Add page3 and page4 modules for navigation in app.py
    
    chore: Update python environment and dependencies
    
    refactor: Update program structure
    
    feat: Add SQL query, API-Endpoint, and bar chart for top selling products
    
    chore: Update database URL in main.py
    
    refactor: Update app.py to use page1 and page2 modules for navigation

--- python/frontend/page1.py
+++ python/frontend/page1.py
@@ -1,25 +1,50 @@
 import streamlit as st
+import plotly.graph_objects as go
 import requests
-import pandas as pd
+
+def fetch_top_pizzas():
+    response = requests.get('http://localhost:8000/top-selling-products')
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        st.error("Fehler beim Abrufen der Daten.")
+        return []
+
+def create_stacked_barchart(data):
+    sizes = sorted(set([item['size'] for item in data]))
+    pizza_names = sorted(set([item['name'] for item in data]))
+
+    sales_by_size = {size: [] for size in sizes}
+    for item in data:
+        sales_by_size[item['size']].append(item['TotalSold'])
+
+    fig = go.Figure()
+
+    for size in sizes:
+        fig.add_trace(go.Bar(
+            x=pizza_names,
+            y=sales_by_size[size],
+            name=size,
+            legendgroup=size,
+            showlegend=True
+        ))
+
+    fig.update_layout(barmode='stack',
+                      title='Sales figures for various pizzas by size',
+                      xaxis_tickangle=-45,
+                      xaxis_title='Pizza Name',
+                      yaxis_title='Total Sold')
+
+    return fig
 
 def main():
-    
-    st.title("Dynamische Datenfilterung")
-
-    filter_text = st.text_input("Filter for state_abbr", "")
-
-    @st.cache_data(ttl=60)
-    def fetch_data(filter: str):
-        response = requests.get("http://localhost:8000/stores/", params={"filter": filter})
-        if response.status_code == 200:
-            return response.json()
-        else:
-            return []
-
-    data = fetch_data(filter_text)
-
-    if data:
-        df = pd.DataFrame(data)
-        st.dataframe(df)
-    else:
-        st.write("Keine Daten gefunden.")
+    st.title("Top selling pizzas")
+    pizzas_data = fetch_top_pizzas()
+    if pizzas_data:
+        fig = create_stacked_barchart(pizzas_data)
+        st.plotly_chart(fig)
+        
+        # Anzeigen der Liste der Pizzen und ihrer Verkaufszahlen
+        st.subheader("List of pizzas and their sales figures:")
+        st.table(pizzas_data)
--- python/frontend/page2.py
+++ python/frontend/page2.py
@@ -1,47 +1,18 @@
 import streamlit as st
-import plotly.graph_objects as go
-import requests
-
-def fetch_top_pizzas():
-    response = requests.get('http://localhost:8000/top-selling-products')
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    else:
-        st.error("Fehler beim Abrufen der Daten.")
-        return []
-
-def create_stacked_barchart(data):
-    sizes = sorted(set([item['size'] for item in data]))
-    pizza_names = sorted(set([item['name'] for item in data]))
-
-    sales_by_size = {size: [] for size in sizes}
-    for item in data:
-        sales_by_size[item['size']].append(item['TotalSold'])
-
-    fig = go.Figure()
-
-    for size in sizes:
-        fig.add_trace(go.Bar(
-            x=pizza_names,
-            y=sales_by_size[size],
-            name=size,
-            legendgroup=size,
-            showlegend=True
-        ))
-
-    fig.update_layout(barmode='stack',
-                      title='Verkaufszahlen verschiedener Pizzas nach Größe',
-                      xaxis_tickangle=-45,
-                      xaxis_title='Pizza Name',
-                      yaxis_title='Total Sold')
-
-    return fig
 
 def main():
-    st.title("Top Verkaufte Pizzen")
-    pizzas_data = fetch_top_pizzas()
-    if pizzas_data:
-        st.write(pizzas_data)
-        fig = create_stacked_barchart(pizzas_data)
-        st.plotly_chart(fig)
+
+# Titel der Streamlit-Seite
+    st.title("Meine erste Streamlit-App")
+
+# Begrüßungstext
+    st.write("Willkommen zu meiner einfachen Streamlit-Seite!")
+
+# Eingabefeld für eine Zahl
+    number = st.number_input("Gib eine Zahl ein", min_value=0)
+
+# Berechnung des Quadrats der eingegebenen Zahl
+    squared_number = number ** 2
+
+# Ausgabe des Ergebnisses
+    st.write(f"Das Quadrat der eingegebenen Zahl ist: {squared_number}")
