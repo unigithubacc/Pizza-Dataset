5087741879c3646deaaee732f697d9f4ca40b177
unigithubacc <aimforthemoon.00@gmail.com>
2024-05-31 23:46:56 +0200

    feat: Refactor stacked barchart creation to improve performance and readability

--- python/frontend/page1.py
+++ python/frontend/page1.py
@@ -1,40 +1,40 @@
 import streamlit as st
 import plotly.graph_objects as go
 import requests
+from collections import defaultdict
 
 def fetch_top_pizzas():
     response = requests.get('http://localhost:8000/top-selling-products')
     if response.status_code == 200:
-        data = response.json()
-        # Sortiere die Daten nach TotalSold innerhalb jeder Größe
-        for size in set([item['size'] for item in data]):
-            data.sort(key=lambda x: x['TotalSold'], reverse=True)
-        return data
+        return response.json()
     else:
         st.error("Fehler beim Abrufen der Daten.")
         return []
 
 def create_stacked_barchart(data):
-    sizes = sorted(set([item['size'] for item in data]))
-    pizza_names = [item['name'] for item in data]
-
-    sales_by_size = {size: [] for size in sizes}
-
+    # Berechne die Summe der Verkäufe für jede Pizza
+    pizza_sales = defaultdict(int)
     for item in data:
-        sales_by_size[item['size']].append(item['TotalSold'])
+        pizza_sales[item['name']] += item['TotalSold']
+
+    # Sortiere die Pizzen nach der Summe der Verkäufe
+    sorted_pizzas = sorted(pizza_sales.items(), key=lambda x: x[1], reverse=True)
+    sorted_pizza_names = [pizza[0] for pizza in sorted_pizzas]
+
+    sizes = sorted(set([item['size'] for item in data]))
+    sales_by_size = {size: [0] * len(sorted_pizza_names) for size in sizes}
+
+    # Ordne die Verkäufe den sortierten Pizzen zu
+    for item in data:
+        index = sorted_pizza_names.index(item['name'])
+        sales_by_size[item['size']][index] = item['TotalSold']
 
     fig = go.Figure()
 
     for size in sizes:
-        # Filtere nur die Elemente dieser Größe
-        filtered_items = [item for item in data if item['size'] == size]
-        # Sortiere die gefilterten Elemente nach TotalSold
-        filtered_items.sort(key=lambda x: x['TotalSold'], reverse=True)
-        # Weise die Pizza-Namen basierend auf ihrer Position in den gefilterten Elementen zu
-        names_for_this_size = [filtered_items[i]['name'] for i in range(len(filtered_items))]
         fig.add_trace(go.Bar(
-            x=names_for_this_size,
-            y=[sales_by_size[size][i] for i in range(len(names_for_this_size))],
+            x=sorted_pizza_names,
+            y=sales_by_size[size],
             name=size,
             legendgroup=size,
             showlegend=True
@@ -58,3 +58,4 @@ def main():
         # Anzeigen der Liste der Pizzen und ihrer Verkaufszahlen
         st.subheader("List of pizzas and their sales figures:")
         st.table(pizzas_data)
+
