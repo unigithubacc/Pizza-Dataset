9852bab1607e56e4ecb10f1584bf81349709b090
unigithubacc <aimforthemoon.00@gmail.com>
2024-06-29 12:14:17 +0200

    feat: Update launch.json configuration
    This commit updates the launch.json configuration file in the .vscode directory. It changes the "name" attribute of the first configuration from "Start Uvicorn" to "Python Debugger: Current File". It also updates the "program" attribute of the second configuration to "C://Users//Uni//ProgrammingLab//Pizza-Dataset//python//dev.py" and adds the "justMyCode" attribute with the value set to true. These changes improve the debugging experience and provide more accurate code execution.

---
+++ python/dev.py
@@ -0,0 +1,11 @@
+from streamlit.web import bootstrap
+
+
+def main():
+    real_script = 'app.py'
+
+    bootstrap.run(real_script, f'run.py {real_script}', [], {})
+
+
+if __name__ == "__main__":
+    main()
