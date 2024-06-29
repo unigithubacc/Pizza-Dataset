a9662516414e8f47dc4383babffe5e29ef9317a4
unigithubacc <aimforthemoon.00@gmail.com>
2024-06-29 21:59:44 +0200

    feat: Add rerun functionality to selected store removal

--- python/frontend/page7.py
+++ python/frontend/page7.py
@@ -1,51 +1,38 @@
+from datetime import date
 import streamlit as st
 import requests
 import plotly.graph_objects as go
 from streamlit_plotly_events import plotly_events
-from datetime import date
 
+# Funktion zum Abrufen der Top-Selling-Stores-Daten
 @st.cache_data
 def fetch_top_selling_stores(start_date, end_date):
     response = requests.get(f'http://localhost:8000/top-selling-stores?start_date={start_date}&end_date={end_date}')
     if response.status_code == 200:
-        return response.json()
+        data = response.json()
+        return data
     else:
         st.error("Fehler beim Abrufen der Daten.")
         return []
 
-@st.cache_data
-def fetch_revenue_data(store_ids, period, end_date):
-    store_id_query = "&".join([f"storeid={store_id}" for store_id in store_ids])
-    url = f'http://localhost:8000/revenue-by-store/?{store_id_query}&period={period}&end_date={end_date}'
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        st.error("Fehler beim Abrufen der Revenue-Daten.")
-        return []
 
-@st.cache_data
-def fetch_sales_data(period, end_date):
-    response = requests.get(f'http://localhost:8000/sales-report-time-interval/?period={period}&end_date={end_date}')
-    if response.status_code == 200:
-        return response.json()
-    else:
-        st.error("Fehler beim Abrufen der Verkaufsdaten.")
-        return []
-
-def create_store_bar_chart(data, selected_store_ids, selected_store_colors, default_color):
+# Function to create bar chart for top-selling stores
+def create_store_bar_chart2(data):
     store_ids = sorted(set([item['storeid'] for item in data]))
-    total_revenue = {store_id: next(item['TotalRevenue'] for item in data if item['storeid'] == store_id) for store_id in store_ids}
+    total_revenue = {store_id: sum(item['TotalRevenue'] for item in data if item['storeid'] == store_id) for store_id in store_ids}
+
     sorted_store_ids = sorted(total_revenue.keys(), key=lambda x: total_revenue[x], reverse=True)
+
     fig = go.Figure()
 
     for store_id in sorted_store_ids:
-        marker_color = selected_store_colors[selected_store_ids.index(store_id)] if store_id in selected_store_ids else default_color
         fig.add_trace(go.Bar(
             x=[store_id],
             y=[total_revenue[store_id]],
             name=f'Store {store_id}',
-            marker_color=marker_color,
+            legendgroup=f'Store {store_id}',
+            showlegend=False,
+            marker_color='lightskyblue',
             customdata=[store_id],
             hoverinfo='x+y'
         ))
@@ -53,116 +40,22 @@ def create_store_bar_chart(data, selected_store_ids, selected_store_colors, defa
     fig.update_layout(barmode='group',
                       title='Top Selling Stores',
                       xaxis_title='Store ID',
-                      yaxis_title='Revenue in $',
+                      yaxis_title='Total Revenue in $',
                       clickmode='event+select',
                       width=800,
                       height=400)
 
     return fig
 
-def create_revenue_line_chart(data, store_ids, store_colors, period):
-    fig = go.Figure()
-    for i, store_id in enumerate(store_ids):
-        store_data = [item for item in data if item['storeid'] == store_id]
-        if store_data:
-            x_values = [item['period'] for item in store_data]
-            total_revenue = [item['total_revenue'] for item in store_data]
-            fig.add_trace(go.Scatter(
-                x=x_values,
-                y=total_revenue,
-                mode='lines+markers',
-                name=f'Store {store_id}',
-                line=dict(color=store_colors[i])
-            ))
 
-    fig.update_layout(title='Revenue for Selected Stores',
-                      xaxis_title=f'{period}',
-                      yaxis_title='Total Revenue',
-                      width=800,
-                      height=400)
-
-    return fig
-
-def create_sales_line_chart(data, store_ids, store_colors, period):
-    fig = go.Figure()
-    for i, store_id in enumerate(store_ids):
-        store_data = [item for item in data if item['storeid'] == store_id]
-        if store_data:
-            x_values = [item['period'] for item in store_data]
-            total_sales = [item['total_sales'] for item in store_data]
-            fig.add_trace(go.Scatter(
-                x=x_values,
-                y=total_sales,
-                mode='lines+markers',
-                name=f'Store {store_id}',
-                line=dict(color=store_colors[i])
-            ))
-
-    fig.update_layout(title='Number of Sales for Selected Stores',
-                      xaxis_title=f'{period}',
-                      yaxis_title='Total Sales',
-                      width=800,
-                      height=400)
-
-    return fig
-
-def create_empty_line_chart():
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=[], y=[], mode='lines+markers', name='No data'))
-    fig.update_layout(title='Number of Sales for Store',
-                      xaxis_title='Period',
-                      yaxis_title='Number of Sales',
-                      width=800,
-                      height=400)
-    return fig
 
 def main():
     start_date = st.sidebar.date_input("Start Date", value=date(2020, 1, 1))
-    end_date = st.sidebar.date_input("End Date", value=date(2023, 1, 1))
-    period = st.sidebar.selectbox("Select Period", ["Day", "Month", "Quarter", "Year"], index=3)
+    end_date = st.sidebar.date_input("End Date", value=date(2023, 1, 1))    
+    st.title("Top Selling Stores")
 
-    if 'top_selling_stores' not in st.session_state or st.session_state.start_date != start_date or st.session_state.end_date != end_date:
-        st.session_state.start_date = start_date
-        st.session_state.end_date = end_date
-        st.session_state.top_selling_stores = fetch_top_selling_stores(start_date, end_date)
-
-    top_selling_stores = st.session_state.top_selling_stores
-
-    if top_selling_stores:
-        if 'selected_store_ids' not in st.session_state:
-            st.session_state.selected_store_ids = []
-            st.session_state.selected_store_colors = []
-
-        color_palette = ['#f781bf', '#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628']
-        default_color = 'lightskyblue'
-
-        fig = create_store_bar_chart(top_selling_stores, st.session_state.selected_store_ids, st.session_state.selected_store_colors, default_color)
+    stores_data = fetch_top_selling_stores(start_date, end_date)
+    if stores_data:
+        fig = create_store_bar_chart2(stores_data)
         selected_points = plotly_events(fig, click_event=True)
 
-        if selected_points:
-            new_store_id = selected_points[0]['x']
-            if new_store_id in st.session_state.selected_store_ids:
-                index = st.session_state.selected_store_ids.index(new_store_id)
-                st.session_state.selected_store_ids.pop(index)
-                st.session_state.selected_store_colors.pop(index)
-            else:
-                st.session_state.selected_store_ids.append(new_store_id)
-                st.session_state.selected_store_colors.append(color_palette[len(st.session_state.selected_store_ids) % len(color_palette)])
-
-            # Aktualisiere das Balkendiagramm nach der Auswahländerung
-            fig = create_store_bar_chart(top_selling_stores, st.session_state.selected_store_ids, st.session_state.selected_store_colors, default_color)
-
-        if st.session_state.selected_store_ids:
-            revenue_data = fetch_revenue_data(st.session_state.selected_store_ids, period, end_date)
-            revenue_fig = create_revenue_line_chart(revenue_data, st.session_state.selected_store_ids, st.session_state.selected_store_colors, period)
-            st.plotly_chart(revenue_fig, use_container_width=False)
-
-            sales_data = fetch_sales_data(period, end_date)
-            sales_fig = create_sales_line_chart(sales_data, st.session_state.selected_store_ids, st.session_state.selected_store_colors, period)
-            st.plotly_chart(sales_fig, use_container_width=False)
-        else:
-            empty_fig = create_empty_line_chart()
-            st.plotly_chart(empty_fig, use_container_width=False)
-            st.sidebar.warning("Select store IDs to see the number of sales for those stores")
-    else:
-        st.error("No top selling stores data available.")
